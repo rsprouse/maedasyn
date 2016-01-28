8bcd17ba1396bfbce8efe6c33bac7d7317871650
Oski Bear <oski@bear>
2016-01-28 12:01:08 -0800

    Revert "Fix package import."
    This reverts commit bc7db3ba8e696fe340c7187f62818aca3ef0ccf2.

--- setup.py
+++ setup.py
@@ -5,32 +5,12 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
-# The numpy.get_include() is often needed for compiling cython modules, but
-# synth.pyx doesn't seem to need it, and the include_dirs line caused an
-# import error in which
-#
-#   import maedasyn.synth
-#
-# resulted in the error
-#
-#   ImportError: No module named synth
-#
-# There was a workaround for this situation. This worked:
-#
-#   import numpy as np
-#   import maedasyn.synth
-#
-# Removing the include_dirs line makes the workaround unnecessary and makes
-# it possible to do import maedasyn.synth without importing numpy first.
-# 
-# If we ever need to revert to using include_dirs in order for compilation
-# to succeed, we'll want to recall the workaround.
 ext_modules = [
   Extension(
     name="maedasyn.synth",
     sources=["maedasyn/synth.pyx"],
     libraries = ["m"],
-#    include_dirs=[numpy.get_include()],
+    include_dirs=[numpy.get_include()],
     language="c",
   )
 ]
